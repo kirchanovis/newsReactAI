74d2200d3187b6eda0742cf82b7a052dd6b4818a
kirchanov_is <ilya.kirchanov@open.ru>
2023-03-23 18:56:39 +0300

    (ai) add Header component

--- src/containers/SiteLayout/SiteLayout.tsx
+++ src/containers/SiteLayout/SiteLayout.tsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { Header } from "../Header";
 import "./styles.css";
 
 function SiteLayout() {
   return (
     <div className="site-layout">
-      <header>
-        <h1>Заголовок сайта</h1>
-      </header>
+      <Header />
       <main>
         <p>Основное содержимое сайта здесь</p>
       </main>
