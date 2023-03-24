4247fdf80bea3f2de9471f1815d64915ce2b59af
kirchanov_is <ilya.kirchanov@open.ru>
2023-03-24 10:11:58 +0300

    (ai) update Header, add Menu

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,31 +1,18 @@
 import React from "react";
 import "./styles.css";
+import { Menu } from "../Menu"
 
 function Header() {
   return (
     <header className="site-header">
-      <img
-        src="https://via.placeholder.com/50x150"
-        alt="Логотип сайта"
-        className="site-logo"
-      />
-      <nav className="site-menu">
-        <a href="#" className="menu-link active">
-          Главная
-        </a>
-        <a href="#" className="menu-link">
-          Регион
-        </a>
-        <a href="#" className="menu-link">
-          Статьи
-        </a>
-        <a href="#" className="menu-link">
-          Фоторепортажи
-        </a>
-        <a href="#" className="menu-link">
-          История Ставрополья
-        </a>
-      </nav>
+      <div className="header-logo">
+        <img
+          src="https://via.placeholder.com/50x150"
+          alt="Логотип сайта"
+          className="site-logo"
+        />
+      </div>
+      <Menu />
     </header>
   );
 }
---
+++ src/containers/Menu/Menu.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <nav className="site-menu">
+      <a href="#" className="menu-link active">
+        Главная
+      </a>
+      <a href="#" className="menu-link">
+        Регион
+      </a>
+      <a href="#" className="menu-link">
+        Статьи
+      </a>
+      <a href="#" className="menu-link">
+        Фоторепортажи
+      </a>
+      <a href="#" className="menu-link">
+        История Ставрополья
+      </a>
+    </nav>
+  );
+}
+
+export default Menu;
